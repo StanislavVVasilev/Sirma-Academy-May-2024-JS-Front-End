f93111c9de8f46cef8805b8de12efe97002e438c
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-06 22:23:16 +0300

    add solution task 21.Average Speed

---
+++ 01.Basic Syntax/21.averageSpeed.js
@@ -0,0 +1,10 @@
+function averageSpeed(distance, time) {
+
+    let avgSpeed = distance / time;
+
+    console.log(avgSpeed.toFixed(2));
+}
+
+averageSpeed(200, 2);
+averageSpeed(150, 3);
+averageSpeed(300, 5);
