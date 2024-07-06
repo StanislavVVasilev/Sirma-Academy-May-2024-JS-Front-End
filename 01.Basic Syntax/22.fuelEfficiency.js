75731df45298f7d87b84b2c4e9f58b281c2decce
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-06 22:24:40 +0300

    add solution task 22.Fuel Efficiency

---
+++ 01.Basic Syntax/22.fuelEfficiency.js
@@ -0,0 +1,11 @@
+function fuelEfficiency(distance, fuel) {
+
+    let fuelEff = distance / fuel;
+
+    console.log(fuelEff.toFixed(2));
+
+}
+
+fuelEfficiency(500, 40);
+fuelEfficiency(600, 40);
+fuelEfficiency(750, 40);
