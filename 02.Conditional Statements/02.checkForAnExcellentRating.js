377a2d3dd12f7fff750d4aae10d277c2921212ff
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-07 23:34:27 +0300

    add solution task 02.Check for an excellent rating

---
+++ 02.Conditional Statements/02.checkForAnExcellentRating.js
@@ -0,0 +1,12 @@
+function excellentRating (rating) {
+
+    if (rating >= 5.50) {
+        console.log("Excellent!");
+    }
+
+}
+
+excellentRating(6);
+excellentRating(5);
+excellentRating(5.51);
+excellentRating(5.501);
