0ce85e33ad5202235b7290ca98f8a7bafedd2393
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-07 23:41:19 +0300

    add solution task 07.Even or odd

---
+++ 02.Conditional Statements/07.evenOrOdd.js
@@ -0,0 +1,14 @@
+function evenOrOdd(num) {
+
+    if (num % 2 !== 0) {
+        console.log("odd");
+    } else {
+        console.log("even");
+    }
+
+}
+
+evenOrOdd(2);
+evenOrOdd(3);
+evenOrOdd(25);
+evenOrOdd(1024);
