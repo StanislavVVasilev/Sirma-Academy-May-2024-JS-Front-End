53c8c1ea39d50625bebb7efb69ce24d67c93167c
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-07 23:36:20 +0300

    add solution task 03.Larger number

---
+++ 02.Conditional Statements/03. largerNumber.js
@@ -0,0 +1,12 @@
+function largerNumber(firstNum, secondNum) {
+
+    if (firstNum > secondNum) {
+        console.log(firstNum);
+    } else {
+        console.log(secondNum);
+    }
+}
+
+largerNumber(2, 4);
+largerNumber(7, 12);
+largerNumber(-1, -5);
