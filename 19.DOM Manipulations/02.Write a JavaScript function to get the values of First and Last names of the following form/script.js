e63b8a0fb4f8be945bb5f462f7f2750f4042cfea
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-09 16:38:41 +0300

    add solution task 02.Write a JavaScript function to get the values of First and Last names of the following form

---
+++ 19.DOM Manipulations/02.Write a JavaScript function to get the values of First and Last names of the following form/script.js
@@ -0,0 +1,8 @@
+function getFormvalue() {
+
+    const firstNameEl = document.querySelector('input[name=fname]');
+    const lastNameEl = document.querySelector('input[name=lname]');
+    
+    console.log(firstNameEl.value);
+    console.log(lastNameEl.value);
+}
