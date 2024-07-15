5f9bd30ae4d6f2517af97d969d51e402240ae080
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-15 05:39:04 +0300

    add solution task 03.Numbers 1 to N through 2

---
+++ 03. Loops/03.numbers1ToNThrough2.js
@@ -0,0 +1,9 @@
+function numbersFrom1ToNThrough2(number) {
+
+    for (let i = 1; i <= number; i += 2) {
+        console.log(i);
+    }
+
+}
+numbersFrom1ToNThrough2(10);
+numbersFrom1ToNThrough2(5);
