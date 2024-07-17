c6d6dee8beccfbb74837af08527e4616d1e13957
StanislavVVasilev <stanislav.vasilev666@gmail.com>
2024-07-17 04:53:15 +0300

    add solution task 23.Numbers up to 1000, ending in n

---
+++ 03. Loops/23.numbersUpTo1000EndingInN.js
@@ -0,0 +1,9 @@
+function numbersUpTo1000EndingInN(num) {
+    
+    for (let i = num; i <= 1000; i += 10) {
+        console.log(i);
+    }
+}
+
+numbersUpTo1000EndingInN(6);
+numbersUpTo1000EndingInN(8);
